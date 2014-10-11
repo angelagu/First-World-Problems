aa9538e0cb7a609ad56446f16d43d8872df7ddf3
Angela <agu@hearsaycorp.com>
2014-10-10 21:46:12 -0700

    backend

--- app/models/first_world_problem.rb
+++ app/models/first_world_problem.rb
@@ -1,2 +1,13 @@
 class FirstWorldProblem < ActiveRecord::Base
+
+  def create_problem(message)
+    problem = FirstWorldProblem.create(problem: message)
+  end
+
+  def get_random_response
+    responses = []
+    file_path = File.join(Rails.root, 'responses.txt')
+    texts = File.read(file_path).split /[\n]/
+    texts.shuffle[0] 
+  end  
 end
